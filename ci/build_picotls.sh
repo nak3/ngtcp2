335f8804cb14ce685747e30935e294df0f3e23a8
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2024-06-16 18:49:15 +0900

    Fix mistake

--- ci/build_picotls.sh
+++ ci/build_picotls.sh
@@ -3,7 +3,7 @@
 
 if [ "${OPENSSL}" = "openssl1" ]; then
     WORKSPACE=picotls-openssl1
-elif [ "${OPENSSL}" = "openssl1" ]; then
+elif [ "${OPENSSL}" = "openssl3" ]; then
     WORKSPACE=picotls-openssl3
 else
     WORKSPACE=picotls-libressl
@@ -18,7 +18,7 @@ git checkout "6f3bfe4b4ddcedfcdeed90888b4f07141ed3b641"
 git submodule update --init --depth 1
 if [ "${OPENSSL}" = "openssl1" ]; then
     PKG_CONFIG_PATH=$PWD/../openssl1/build/lib/pkgconfig
-elif [ "${OPENSSL}" = "openssl1" ]; then
+elif [ "${OPENSSL}" = "openssl3" ]; then
     PKG_CONFIG_PATH=$PWD/../openssl3/build/lib/pkgconfig:$PWD/../openssl3/build/lib64/pkgconfig
 else
     PKG_CONFIG_PATH=$PWD/../libressl/build/lib/pkgconfig:$PWD/../libressl/build/lib/pkgconfig
